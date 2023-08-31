bbae81f4a962eb8948c7d844ed392e6a9142256f
CodeMonk21 <jagritmadaan2001@gmail.com>
2023-08-31 12:06:21 +0530

    added custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    // Some useful comment
+    fontFamily: {
+      'barlow': ['Barlow', 'sans-serif']// Ensure fonts with spaces have " " surrounding it.
+    },
     extend: {},
   },
   plugins: [],
